4beb1aad803cb5f13f39bb6c7b3bd8508dce9dc6
yuki yamamoto <yukiyamamoto@yuki-no-macbook-pro.local>
2017-01-08 23:03:53 +0900

    Initial Commit

---
+++ AVAudioApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVAudioApp
+//
+//  Created by yuki yamamoto on H29/01/08.
+//  Copyright © 平成29年 yuki yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
